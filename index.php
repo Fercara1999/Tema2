5c7730af9fd9a36a2f026cae158cd25101717012
Fernando <fercara1999@gmail.com>
2023-09-28 16:58:37 +0000

    Cambios 28/09

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas Clase Tema 2</title>
+</head>
+<body>
+    <p>
+        Echo
+    </p>
+    <?php
+        // Tres maneras diferentes de escribir el mismo contenido
+        echo "Hola Mundo";
+        echo "<br>Hola Mundo","Junto";
+        print ("<br>Hola Mundo con print");
+
+        // Print r para escribir arrays
+        // Printf para imprimir una cadena
+        // Mejor echo
+
+        // 17,99
+        printf ("<br>%s","17,99");
+        printf ("<br>%d","17,99");
+        printf ("<br>%f","17.99");
+
+
+    ?>
+</body>
+</html>
